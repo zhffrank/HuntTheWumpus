d71906a4ed98a0fdb307676597eeda481d35a5a4
Frank Z <zhffrank77@yahoo.com>
2020-07-28 18:18:35 -0700

    add doors for all the rooms properly

--- Console/src/Graphics.cpp
+++ Console/src/Graphics.cpp
@@ -16,33 +16,28 @@ namespace Graphics
 	// room 1,4 | 2,4 | 3,4 | 4,4
 	// printf("=================================\n");
 	// printf("|       |       |       |       |\n");
+	// printf("                                |\n");
 	// printf("|       |       |       |       |\n");
+	// printf("==== ======= ======= ======= ====\n");
 	// printf("|       |       |       |       |\n");
-	// printf("=================================\n");
+	// printf("|                               |\n");
 	// printf("|       |       |       |       |\n");
+	// printf("==== ======= ======= ======= ====\n");
 	// printf("|       |       |       |       |\n");
+	// printf("|                               |\n");
 	// printf("|       |       |       |       |\n");
-	// printf("=================================\n");
+	// printf("==== ======= ======= ======= ====\n");
 	// printf("|       |       |       |       |\n");
-	// printf("|       |       |       |       |\n");
-	// printf("|       |       |       |       |\n");
-	// printf("=================================\n");
-	// printf("|       |       |       |       |\n");
-	// printf("|       |       |       |       |\n");
+	// printf("|                               |\n");
 	// printf("|       |       |       |       |\n");
 	// printf("=================================\n");
 
 	void display_rooms_in_row(int row, const GameLocations& info)
 	{
 		for (int x = 1; x <= info.GetMapX(); x++)
-			printf("========");
-		printf("=\n");
-
-		for (int x = 1; x <= info.GetMapX(); x++)
 			printf("|       ");
 		printf("|\n");
 
-		// 	printf("|   %c    |   %c    |   %c    |   %c    |\n");
 		for (int x = 1; x <= info.GetMapX(); x++) {
 			char object = ' ';
 			if (x == info.m_Player.room_x && row == info.m_Player.room_y) {
@@ -53,7 +48,16 @@ namespace Graphics
 				if (info.m_Wumpus.show)
 					object = info.m_Wumpus.icon;
 			}
-			printf("|   %c   ", object);
+			
+			// for the most left room set all walls
+			// except the room 1 as it is the entrance
+			// all other rooms should connected by default
+			if (x == 1 && row != 1) {
+				printf("|   %c   ", object);
+			}
+			else {
+				printf("    %c   ", object);
+			}
 		}
 		printf("|\n");
 
@@ -64,14 +68,26 @@ namespace Graphics
 
 	void display_rooms(const GameLocations& info)
 	{
-		for (int y = 1; y <= info.GetMapY(); y++) {
-			// the row
-			display_rooms_in_row(y, info);
-		}
-
 		for (int x = 1; x <= info.GetMapX(); x++)
 			printf("========");
 		printf("=\n");
+
+		for (int y = 1; y <= info.GetMapY(); y++) {
+			// the row
+			display_rooms_in_row(y, info);
+
+			for (int x = 1; x <= info.GetMapX(); x++) {
+				// if it is last row, we draw all solid wall
+				// else draw a wall with door
+				if (y == info.GetMapY()) {
+					printf("========");
+				}
+				else {
+					printf("===  ===");
+				}
+			}
+			printf("=\n");
+		}
 	}
 
 	void DisplayGameMap(const GameLocations& info)
